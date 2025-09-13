7f60868adb4789347167a16f0fa962db4be199b0
Dirk Do <dtduy2014@gmail.com>
2025-09-13 23:15:05 +0700

    add

--- day8/astro.config.mjs
+++ day8/astro.config.mjs
@@ -7,13 +7,15 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321", // dev local
+  site: "https://astrojs-test-orpin.vercel.app", // dev local
   // Khi deploy thật thì đổi thành https://ten-mien-cua-ban.com
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap()],
 });
